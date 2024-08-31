c0b5db83df3ab1ca77922e477610003f5d7958b4
Fatmaz04 <118017527+Fatmaz04@users.noreply.github.com>
2024-08-31 18:53:24 +0300

    Update FreelancerIcon.jsx

--- src/components/FreelancerIcon/FreelancerIcon.jsx
+++ src/components/FreelancerIcon/FreelancerIcon.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import profile from "../../assets/profile.png";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Stars from "../Stars/Stars";
 
 const FreelancerIcon = ({ freelancer }) => {
-
   return (
     <div className="border border-[#B8B8BF] rounded-lg p-2 w-[300px] h-[250px] m-2 flex flex-col">
       <div className="flex flex-row mb-4">
@@ -17,11 +16,13 @@ const FreelancerIcon = ({ freelancer }) => {
           <h3 className="font-semibold">{freelancer.name}</h3>
           <h4 className="text-[#797987] text-sm">{freelancer.category}</h4>
           <h4 className="text-[#797987] text-sm">{freelancer.job}</h4>
-          <Stars rating={freelancer.rating}/>
+          <Stars rating={freelancer.rating} />
         </div>
       </div>
       <p className="flex-1 overflow-hidden text-ellipsis">
-        {freelancer.description}
+        {freelancer.description.length > 100
+          ? `${freelancer.description.slice(0, 100)}...`
+          : freelancer.description}
       </p>
       <Link to={`/profile/${freelancer.id}`}>
         <button className="bg-[#037C6A] text-white p-2 px-4 rounded-lg w-full mt-auto">
